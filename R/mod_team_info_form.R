944b71bb3f592a63c4bf5213f8c7558fc4bb8e71
Janith Wanniarachchi <janithcwanni@gmail.com>
2022-02-15 04:18:09 +0530

    push team info to gsheet

--- R/mod_team_info_form.R
+++ R/mod_team_info_form.R
@@ -7,21 +7,52 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
-mod_team_info_form_ui <- function(id){
+mod_team_info_form_ui <- function(id,team_letter){
   ns <- NS(id)
+  POSITIONS <- c("C","GD","WD","WA","GA","GS","GK")
+  POSITIONS <- c(POSITIONS,paste("Substitute",POSITIONS))
+  name_tib <- read_gsheet("Player information")
   tagList(
-    fluidRow(h1("Team Information")),
-    fluidRow(column(6,h1("Team A")),column(6,h1("Team B")))
+    fluidRow(column(12,h1(paste("Team ",team_letter)),
+                    textInput(ns("team_name"),"Team Name"),
+                    lapply(1:14,function(i){
+                      fluidRow(
+                        column(6,selectInput(ns(paste0("t",team_letter,"_p",i)),
+                                             paste("Player",i,"Name"),
+                                             choices = name_tib$Name)),
+                        column(6,selectInput(ns(paste0("t",team_letter,"_pos",i)),
+                                             paste("Player",i,"Position"),
+                                    choices = POSITIONS,width = "50%",selected = POSITIONS[i]))
+                      )
+                    }),
+                    actionButton(ns("submit"),"Submit")
+                    )
+             )
   )
 }
     
 #' team_info_form Server Functions
 #'
 #' @noRd 
-mod_team_info_form_server <- function(id){
+mod_team_info_form_server <- function(id,team_letter){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+    observeEvent(input$submit,{
+      req(input$team_name)
+      team_name <- input$team_name
+      
+      col_df <- data.frame("Team_letter","Team_name",
+                           "Player_name","Position")
+      add_wsheet("Team Information",col_df)
+      
+      for(i in 1:14){
+        player_name <- input[[paste0("t",team_letter,"_p",i)]]
+        position <- input[[paste0("t",team_letter,"_pos",i)]]
+        row <- data.frame(Team_letter = team_letter,Team_name = team_name,
+                          Player_name = player_name,Position = position)
+        append_gsheet(sheet = "Team Information",row)
+      }
+    })
   })
 }
     
