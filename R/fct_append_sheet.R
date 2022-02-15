8e1278831df473af0eccf534d691efc3358119bb
Janith Wanniarachchi <janithcwanni@gmail.com>
2022-02-15 06:55:56 +0530

    update append sheet

--- R/fct_append_sheet.R
+++ R/fct_append_sheet.R
@@ -9,5 +9,12 @@
 #' @noRd
 append_gsheet <- function(sheet_name,df_row){
   ssid <- get_gsheet_spec()
-  googlesheets4::sheet_append(ssid,df_row,sheet=sheet_name)
+  sheet_names <- googlesheets4::sheet_names(ssid)
+  if(sheet_name %in% sheet_names){
+    googlesheets4::sheet_append(ssid,df_row,sheet=sheet_name)
+  }else{
+    print(paste("Creating New Sheet",sheet_name))
+    add_wsheet(sheet_name,data.frame(matrix(colnames(df_row),ncol=ncol(df_row))))
+    googlesheets4::sheet_append(ssid,df_row,sheet=sheet_name)
+  }
 }
